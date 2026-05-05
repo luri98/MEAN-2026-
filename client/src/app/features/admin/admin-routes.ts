4b0811ae9283e9b7cc8f4aa8759d4c1f0574bc9e
Luka Milaš <luka@blackdot.co.me>
2026-05-05 12:06:09 +0200

    Completed Admin Layout

--- client/src/app/features/admin/admin-routes.ts
+++ client/src/app/features/admin/admin-routes.ts
@@ -15,6 +15,18 @@ export const adminRoutes: Routes = [
                 path: 'korisnici',
                 loadComponent: () => import('./pages/users/users').then(m => m.Users)
             },
+            {
+                path: 'kategorije',
+                loadComponent: () => import('./pages/categories/categories').then(m => m.Categories)
+            },
+            {
+                path: 'proizvodi',
+                loadComponent: () => import('./pages/products/products').then(m => m.Products)
+            },
+            {
+                path: 'porudzbine',
+                loadComponent: () => import('./pages/orders/orders').then(m => m.Orders)
+            },
         ]
     }
 ]
---
+++ client/src/app/features/admin/pages/orders/orders.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-orders',
+    imports: [],
+    templateUrl: './orders.html'
+})
+
+export class Orders {
+    
+}
